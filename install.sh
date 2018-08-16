745dc91a3035bd0f81eadc8be67e702f24eb12db
Elliot Wolk <elliot.wolk@gmail.com>
2018-08-16 13:18:45 -0400

    install.sh: only install if compile succeeded

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-gcc chatpop.c -o chatpop.so -fPIC -shared `pkg-config pidgin --cflags --libs glib-2.0`  -Wall -Werror-implicit-function-declaration
-sudo cp chatpop.so /usr/lib/purple-2/
-
+gcc chatpop.c -o chatpop.so \
+  -fPIC -shared `pkg-config pidgin --cflags --libs glib-2.0` \
+  -Wall -Werror-implicit-function-declaration \
+  && sudo cp chatpop.so /usr/lib/purple-2/
