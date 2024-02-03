cbc57d5d1e1183fb181444983a0f6a24287b05db
Elliot Wolk <elliot.wolk@gmail.com>
2024-02-03 00:12:53 -0500

    install: mkdir before installing

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 gcc chatpop.c -o chatpop.so \
   -fPIC -shared `pkg-config pidgin --cflags --libs glib-2.0` \
   -Wall -Werror-implicit-function-declaration \
+  && sudo mkdir -p /usr/lib/purple-2/ \
   && sudo cp chatpop.so /usr/lib/purple-2/
